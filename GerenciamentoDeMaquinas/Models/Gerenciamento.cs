faf23244ff33daa3c795b89e26c288dce6689929
legend.rafael <rafaelhenriquedefranca@gmail.com>
2023-05-20 15:10:51 -0300

    Implentação da opção de "Busca de Máquina" com as 3 sub-opções.

--- GerenciamentoDeMaquinas/Models/Gerenciamento.cs
+++ GerenciamentoDeMaquinas/Models/Gerenciamento.cs
@@ -17,7 +17,7 @@ namespace GerenciamentoDeMaquinas.Models
             while (true)
             {
                 Console.Clear();
-                Console.WriteLine(" ------------------ Adicionar Máquina ------------------ ");
+                Console.WriteLine("------------------ Adicionar Máquina ------------------");
 
                 Console.WriteLine("\n\nInforme o número da Estação e Trabalho (PA)");
                 Console.Write(">> ");
@@ -78,41 +78,89 @@ namespace GerenciamentoDeMaquinas.Models
         public void AlterarMaquina()
         {
             Console.Clear();
-            Console.WriteLine(" ------------------ Alterar Máquina ------------------ ");
+            Console.WriteLine("------------------ Alterar Máquina ------------------");
         }
 
         public void RemoverMaquina()
         {
             Console.Clear();
-            Console.WriteLine(" ------------------ Remover Máquina ------------------ ");
+            Console.WriteLine("------------------ Remover Máquina ------------------");
         }
 
         public void BuscarMaquina()
         {
             Console.Clear();
-            Console.WriteLine(" ------------------ Buscar Máquina ------------------ ");
+            Console.WriteLine("------------------ Buscar Máquina ------------------");
 
-            Console.WriteLine("\n\nInforme a forma que deseja buscar a máquina");
-            Console.WriteLine("1 - Estação de Trabalho (PA)\n2 - Nome Lógico\n3 - Número de Série");
+            Console.WriteLine("\n\nQual a forma que deseja buscar a máquina\n");
+            Console.WriteLine("1 - Estação de Trabalho (PA)\n2 - Nome Lógico\n3 - Número de Série\n");
             Console.Write(">> ");
             string opcao = Console.ReadLine();
 
             switch (opcao)
             {
                 case "1":
-
+                    Console.WriteLine("\n----------------------------------------------------");
+                    Console.WriteLine("\nInforme o número da Estação de Trabalho(PA) da máquina para consulta");
+                    Console.Write(">> ");
+                    int estacaoTrabalhoProcurando = int.Parse(Console.ReadLine());
+                    Maquina estacaoTrabalhoEncontrado = maquinas.Find(x => x.EstacaoDeTrabalho == estacaoTrabalhoProcurando);
+                    if (estacaoTrabalhoEncontrado != null)
+                    {
+                        Console.WriteLine($"\n\nInformações sobre a Estação de Trabalho(PA): {estacaoTrabalhoProcurando}\n");
+                        Console.WriteLine($"PA: {estacaoTrabalhoEncontrado.EstacaoDeTrabalho}\n" +
+                                          $"Nome Lógico: {estacaoTrabalhoEncontrado.NomeLogico}\n" +
+                                          $"Número de Série: {estacaoTrabalhoEncontrado.NumeroSerie}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"\nEstação de Trabalho(PA): {estacaoTrabalhoProcurando} está incorreto!");
+                    }
+                    Console.ReadKey();
                     break;
 
                 case "2":
-                    Console.WriteLine("Informe o Nome Lógico");
+                    Console.WriteLine("\n----------------------------------------------------");
+                    Console.WriteLine("\nInforme o Nome Lógico da máquina para consulta");
+                    Console.Write(">> ");
                     string nomeLogicoProcurando = Console.ReadLine();
-
+                    Maquina nomeLogicoEncontrado = maquinas.Find(x => x.NomeLogico == nomeLogicoProcurando);
+                    if (nomeLogicoEncontrado != null )
+                    {
+                        Console.WriteLine($"\n\nInformações sobre o Nome Lógico: {nomeLogicoProcurando}\n");
+                        Console.WriteLine($"PA: {nomeLogicoEncontrado.EstacaoDeTrabalho}\n" +
+                                          $"Nome Lógico: {nomeLogicoEncontrado.NomeLogico}\n" +
+                                          $"Número de Série: {nomeLogicoEncontrado.NumeroSerie}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"\nNome Lógico: {nomeLogicoProcurando} está incorreto!");
+                    }
+                        Console.ReadKey();
                     break;
 
                 case "3":
+                    Console.WriteLine("\n----------------------------------------------------");
+                    Console.WriteLine("\nInforme o Número de Série da máquina para consulta");
+                    Console.Write(">> ");
+                    string numeroSerieProcurando = Console.ReadLine();
+                    Maquina numeroSerieEncontrado = maquinas.Find(x => x.NumeroSerie == numeroSerieProcurando);
+                    if (numeroSerieEncontrado != null)
+                    {
+                        Console.WriteLine($"\n\nInformações sobre o Número de Série: {numeroSerieProcurando}\n");
+                        Console.WriteLine($"PA: {numeroSerieEncontrado.EstacaoDeTrabalho}\n" +
+                                          $"Nome Lógico: {numeroSerieEncontrado.NomeLogico}\n" +
+                                          $"Número de Série: {numeroSerieEncontrado.NumeroSerie}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"\nNúmero de Série: {numeroSerieProcurando} está incorreto!");
+                    }
+                    Console.ReadKey();
                     break;
 
                 default:
+                    Console.WriteLine("Favor, digite uma opção válida.");
                     break;
 
             }
@@ -121,12 +169,14 @@ namespace GerenciamentoDeMaquinas.Models
         public void ListarTodasMaquinas()
         {
             Console.Clear();
-            Console.WriteLine(" ------------------ Listar Todas Máquina ------------------ \n\n");
+            Console.WriteLine("------------------ Listar Todas Máquina ------------------");
+            Console.WriteLine("\n");
 
             foreach (Maquina maquina in maquinas)
             {
-                Console.Write($"PA: {maquina.EstacaoDeTrabalho} | Nome Lógico: {maquina.NomeLogico} | Número de Série: {maquina.NumeroSerie}");
+                Console.WriteLine($"PA: {maquina.EstacaoDeTrabalho} | Nome Lógico: {maquina.NomeLogico} | Número de Série: {maquina.NumeroSerie}");
             }
+            Console.ReadKey();
         }
 
         static bool VerificarMaquina(List<Maquina> maquinas, int pa)
@@ -134,18 +184,6 @@ namespace GerenciamentoDeMaquinas.Models
             foreach (Maquina maquina in maquinas)
             {
                 if (maquina.EstacaoDeTrabalho == pa)
-                {
-                    return true;
-                }
-            }
-
-            return false;
-        }
-        static bool VerificarMaquina(List<Maquina> maquinas, string procura)
-        {
-            foreach (Maquina maquina in maquinas)
-            {
-                if (maquina.NomeLogico == procura)
                 {
                     return true;
                 }
