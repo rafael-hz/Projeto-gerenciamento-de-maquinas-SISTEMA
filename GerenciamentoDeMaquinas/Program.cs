62766f012c8fc72f924ed1bf5f4435d2dffbae67
Rafael Henrique <64560384+rafael-hz@users.noreply.github.com>
2023-05-19 21:59:45 -0300

    Criação de Menu

--- GerenciamentoDeMaquinas/Program.cs
+++ GerenciamentoDeMaquinas/Program.cs
@@ -6,9 +6,10 @@ Gerenciamento gerenciamento = new Gerenciamento();
 
 while (exibirMenu)
 {
+    Console.Clear();
     Console.WriteLine("Seja bem vindo!");
     Console.WriteLine("Digite um número para navegar no sistema");
-    Console.WriteLine("\n1 - Adicionar Máquina\n2 - Alterar Máquina\n3 - Remover Máquina\n4 - Buscar Máquina\n5 - Listar Todas Máquinas");
+    Console.WriteLine("\n1 - Adicionar Máquina\n2 - Alterar Máquina\n3 - Remover Máquina\n4 - Buscar Máquina\n5 - Listar Todas Máquinas\n6 - Sair\n");
     Console.Write(">> ");
     string opcao = Console.ReadLine();
 
@@ -19,15 +20,28 @@ while (exibirMenu)
             break;
 
         case "2":
+            gerenciamento.AlterarMaquina();
             break;
 
         case "3":
+            gerenciamento.RemoverMaquina();
             break;
 
         case "4":
+            gerenciamento.BuscarMaquina();
             break;
 
         case "5":
+            gerenciamento.ListarTodasMaquinas();
+            break;
+
+        case "6":
+            exibirMenu = false;
+            break;
+
+        default:
+            Console.WriteLine("\nFavor, digitar um número válido");
+            Console.ReadKey();
             break;
     }
 }
